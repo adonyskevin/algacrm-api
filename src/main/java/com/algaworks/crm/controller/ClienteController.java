52d5178b2d1b51f4f95cbde63c36ed1624c3b2b6
Adonys Kevin <adonyskevinms@gmail.com>
2022-03-14 23:28:25 -0300

    Alteração do método adicionar e criação dos métodos listar por Id e excluir

--- src/main/java/com/algaworks/crm/controller/ClienteController.java
+++ src/main/java/com/algaworks/crm/controller/ClienteController.java
@@ -1,10 +1,14 @@
 package com.algaworks.crm.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +30,31 @@ public class ClienteController {
 		return clienteRepository.findAll();
 	}
 	
+	@GetMapping("/{id}")
+	public Optional<Cliente> listarClientePorId(@PathVariable Long id) {
+		return clienteRepository.findById(id);
+	}
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
-		return clienteRepository.save(cliente);
+	public List<Cliente> adicionar(@RequestBody List<Cliente> clientes) {
+		List<Cliente> clientesSalvos = new ArrayList<Cliente>();
+		for (Cliente cliente : clientes) {
+			clientesSalvos.add(clienteRepository.save(cliente));
+		}
+		
+		return clientesSalvos;
+	}
+	
+	@DeleteMapping("/{id}")
+	public String excluirCliente(@PathVariable Long id) {
+		try {
+			Cliente cliente = clienteRepository.getById(id);
+			clienteRepository.delete(cliente);
+			
+			return "Cliente " + cliente.getNome() + " excluído com sucesso!";
+		} catch (RuntimeException e) {
+			return "Ocorreu um erro ao excluir o cliente";
+		}
 	}
 }
